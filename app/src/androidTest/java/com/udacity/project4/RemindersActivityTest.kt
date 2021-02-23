30a31dc7754bb81fa88b7f09b139409d31bf68b9
John Vieira <jvieira575@gmail.com>
2021-02-22 22:57:28 -0600

    Added ReminderListFragmentTest.

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,30 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -21,12 +35,17 @@ import org.koin.test.get
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-//END TO END test to black box test the app
-class RemindersActivityTest :
-    AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
+class RemindersActivityTest : AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private lateinit var reminderDTO : ReminderDTO
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+    private lateinit var viewModel: SaveReminderViewModel
+
+    @Rule
+    @JvmField
+    var grantPermissionRule = GrantPermissionRule.grant("android.permission.ACCESS_FINE_LOCATION")
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -58,6 +77,7 @@ class RemindersActivityTest :
         }
         //Get our real repository
         repository = get()
+        viewModel = get()
 
         //clear the data to start fresh
         runBlocking {
@@ -65,7 +85,52 @@ class RemindersActivityTest :
         }
     }
 
+    @Before
+    fun registerIdlingResources() {
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
 
-//    TODO: add End to End testing to the app
+    @After
+    fun unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
 
+    @Test
+    fun addReminder() = runBlocking {
+
+        // Create one reminder
+        reminderDTO = ReminderDTO("Reminder Title", "Reminder Description","Null Island", 0.0, 0.0)
+        repository.saveReminder(reminderDTO)
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Assert our test reminder is displayed in the list
+        onView(withText(reminderDTO.title)).check(matches(isDisplayed()))
+        onView(withText(reminderDTO.description)).check(matches(isDisplayed()))
+        onView(withText(reminderDTO.location)).check(matches(isDisplayed()))
+
+        // Enter a location
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(typeText("New Reminder"))
+        onView(withId(R.id.reminderDescription)).perform(typeText("New Reminder Text"))
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.google_map)).perform(longClick())
+        onView(withId(R.id.save_button)).perform(click())
+
+        // Assert save reminder contains new reminder
+        onView(withText("New Reminder")).check(matches(isDisplayed()))
+        onView(withText("New Reminder Text")).check(matches(isDisplayed()))
+        onView(withText("Dropped Pin")).check(matches(isDisplayed()))
+
+        // Save reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // Assert new reminder is displayed in recycler view
+        onView(withText("New Reminder")).check(matches(isDisplayed()))
+        onView(withText("New Reminder Text")).check(matches(isDisplayed()))
+        onView(withText("Dropped Pin")).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
 }
